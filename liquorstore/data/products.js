ce03788b25bf0abdb9558d8b7a300cb87df9aa55
Muasyafrank <muasyafrank39@gmail.com>
2025-05-25 22:03:22 +0300

    Add files via upload

---
+++ liquorstore/data/products.js
@@ -0,0 +1,146 @@
+const products =[
+    {
+        image:"images/brandy.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/gin.jpg",
+        tag:"Best Seller",
+        name:"Gin",
+        description:"Jim Beam Kentucky Straight",
+        priceCents:5900
+
+    },
+     {
+        image:"images/rum.jpg",
+        tag:"New Arrival",
+        name:"Rum",
+        description:"Citadelle",
+        priceCents:6900
+
+    }, {
+        image:"images/tequila.jpg",
+        tag:"sale",
+        name:"Tequilla",
+        description:"Jose Quervo",
+        priceCents:6900
+
+    },
+     {
+        image:"images/whiskey.jpg",
+        tag:"Best Seller",
+        name:"Whiskey",
+        description:"Johnny Walker",
+        priceCents:6900
+
+    },
+     {
+        image:"images/vodka.jpg",
+        tag:"new arrival",
+        name:"Vodka",
+        description:"Old Monk",
+        priceCents:6900
+
+    },
+     {
+        image:"images/kind-1.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/kind-2.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/kind-3.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/kind-4.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/prod-1.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/prod-2.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/prod-3.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/prod-4.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/prod-5.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/prod-6.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/prod-7.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    },
+     {
+        image:"images/prod-8.jpg",
+        tag:"sale",
+        name:"Brandy",
+        description:"Barcardi 151",
+        priceCents:6900
+
+    }
+
+]
